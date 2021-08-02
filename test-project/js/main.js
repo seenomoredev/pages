ec28f913d5dc2b1e87b71370c99b3dd5053b865a
seenomoredev <seenomore@yandex.ru>
2021-08-02 23:08:25 +0700

    test-project

---
+++ test-project/js/main.js
@@ -0,0 +1,32 @@
+$('.select').click(function () {
+    if ($(".select").hasClass("select_opened")) {
+        $('.select').removeClass("select_opened");
+    } else {
+        $('.select').addClass("select_opened");
+    }
+});
+
+$('.select__option').click(function () {
+    $('.select__option').removeClass("select__option_current");
+    $(this).addClass("select__option_current");
+    $('input[name="system"]').val( $(this).text() );
+    var res = $('input[name="system"]').val();
+    $('.select__name').html( $(this).text() );
+    console.log( res );
+});
+
+$( ".slider__slider" ).slider({
+    animate: "slow",
+    range: "min", 
+    min: 0,
+    max: 100,
+    value: 75,
+    step: 1,
+    slide: function( event, ui ) {
+        var sliderval = $( ".slider__slider" ).slider( "values" );
+        $(".slider__value span").text(ui.value);
+        $('input[name="system"]').val(ui.value);
+        console.log( $('input[name="system"]').val() );
+    }
+});
+
